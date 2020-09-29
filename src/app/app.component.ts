e002e400125afcfee07c57bd65b7daaad985b159
Artem Zamyatnin <lololosha102@gmail.com>
2020-09-29 15:32:29 +0300

    Auto stash before rebase of "Tsugan/master"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
 export class AppComponent {
-  title = 'my-app';
+firstOp:number;
+secondOp:number;
+op:string;
+result:number;
+errorMes:string;
+
+doCalc() {
+if (this.firstOp != null && this.secondOp != null && this.op != null) {
+if (this.op == "-") {
+this.result = this.firstOp - this.secondOp;
+this.errorMes = "Ошибок нет";
+} else if (this.op =="+") {
+this.result = this.firstOp + this.secondOp;
+this.errorMes = "Ошибок нет";
+} else if(this.op =="/") {
+if (this.secondOp == 0) {
+this.errorMes ="На ноль нельзя делить";
+this.result == null;
+}else {
+this.result = this.firstOp / this.secondOp;
+this.errorMes = "Ошибок нет";
+}
+} else if(this.op =="*") {
+this.result = this.firstOp * this.secondOp;
+this.errorMes = "Ошибок нет";
+}
+}else{
+if(this.firstOp == null){
+this.errorMes =" Нет 1 операнда";
+}else if (this.secondOp == null){
+this.errorMes = "Нет 2 операнда";
+}else if (this.op == null){
+this.errorMes = "Нет оператора действия";
+}
+}
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
+declarations: [
+  AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+imports: [
+  BrowserModule,
+  FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+providers: [],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
